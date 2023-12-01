8283829062da833647cf7c5f3636834ef9f37ad3
yeyh33975 <a15952026052@163.com>
2023-12-01 11:21:42 +0800

    1.支持mybatis的ha

--- servers/src/main/java/com/hundsun/atp/servers/controller/AtpDatasourceCheck.java
+++ servers/src/main/java/com/hundsun/atp/servers/controller/AtpDatasourceCheck.java
@@ -2,6 +2,7 @@ package com.hundsun.atp.servers.controller;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.creator.DataSourceProperty;
 import com.baomidou.dynamic.datasource.creator.DefaultDataSourceCreator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,11 +65,17 @@ public class AtpDatasourceCheck {
         if (CollectionUtil.isNotEmpty(lostedDataSourceMap)) {
             for (Map.Entry<String, DataSource> entry : lostedDataSourceMap.entrySet()) {
                 try {
-                    DataSource dataSource = entry.getValue();
-                    dataSource.getConnection().createStatement().execute("select 1 from dual");
+                    DataSourceProperty dataSourceProperty = new DataSourceProperty();
+                    dataSourceProperty.setUrl("jdbc:mysql://10.20.30.113:33063/atp?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
+                    dataSourceProperty.setDriverClassName("com.mysql.cj.jdbc.Driver");
+                    dataSourceProperty.setUsername("root");
+                    dataSourceProperty.setPassword("root");
+
+                    DataSource newDatasource = dataSourceCreator.createDataSource(dataSourceProperty);
+                    newDatasource.getConnection().createStatement().execute("select 1 from dual");
                     log.info("{} 转为活跃状态Datasource", entry.getKey());
-//                    dataSourceCreator.createDataSource()
-                    ds.addDataSource(entry.getKey(), dataSource);
+
+                    ds.addDataSource(entry.getKey(), newDatasource);
                     lostedDataSourceMap.remove(entry.getKey());
                 } catch (Exception e) {
                     log.error("{} 仍为非活跃状态Datasource ", entry.getKey(), e);
